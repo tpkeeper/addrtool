edb1a7f0b94241ed17d890aad8ad27b291d13ca0
tpkeeper <tpkeeper.me@gmail.com>
2019-09-16 20:40:54 +0800

    add multipubkey

--- address.go
+++ address.go
@@ -1,13 +1,31 @@
 package addrtool
 
 import (
+	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcutil"
 	"github.com/btcsuite/btcutil/base58"
+	"golang.org/x/crypto/ripemd160"
 )
 
 func PubkeyToAddress(key []byte,netId byte)(string){
 	hash160Bytes:=btcutil.Hash160(key)
-	return base58.CheckEncode(hash160Bytes,netId)
+	return base58.CheckEncode(hash160Bytes[:ripemd160.Size],netId)
+}
+
+
+func MultiPubkeyToAddress(netId byte,nRequired int,keys ... []byte) (string ,error){
+	builder:=txscript.NewScriptBuilder().AddInt64(int64(nRequired))
+	for _,key:=range keys{
+		builder.AddData(key)
+	}
+	builder.AddInt64(int64(len(keys)))
+	builder.AddOp(txscript.OP_CHECKMULTISIG)
+	script,err:=builder.Script()
+	if err!=nil{
+		return "",err
+	}
+	scriptHash := btcutil.Hash160(script)
+	return base58.CheckEncode(scriptHash[:ripemd160.Size], netId),nil
 }
 
 
